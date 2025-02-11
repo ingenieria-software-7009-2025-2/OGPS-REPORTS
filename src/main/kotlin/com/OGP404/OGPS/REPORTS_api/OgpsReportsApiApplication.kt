00d10a1c7dbb0db2505debce85fe5ea5e328062c
GeovaniMtz <geovanimartinez14@ciencias.unam.mx>
2025-02-11 17:01:23 -0600

    first commit

---
+++ src/main/kotlin/com/OGP404/OGPS/REPORTS_api/OgpsReportsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.OGP404.OGPS.REPORTS_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OgpsReportsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OgpsReportsApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/OGP404/OGPS/REPORTS_api/OgpsReportsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.OGP404.OGPS.REPORTS_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OgpsReportsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
