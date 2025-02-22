ec8a0671b4d4ad1f19d5b86eb69e21355e96012c
GeovaniMtz <geovanimartinez14@ciencias.unam.mx>
2025-02-21 21:48:13 -0600

    Usuario y UsuarioController, Falta BD

---
+++ src/main/kotlin/com/ogp404/ogps/reports_api/Usuario.kt
@@ -0,0 +1,8 @@
+package com.ogp404.ogps.reports_api
+
+
+data class Usuario(
+    val userName: String = "",
+    val mail: String = "",
+    val password: String = "",
+    )
---
+++ src/main/kotlin/com/ogp404/ogps/reports_api/UsuarioController.kt
@@ -0,0 +1,35 @@
+package com.ogp404.ogps.reports_api
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/usuario")
+class UsuarioController {
+
+    @PostMapping("/create")
+    fun createUsuario(@RequestBody create: Usuario): ResponseEntity<Usuario>{
+        return ResponseEntity.ok(create)
+    }
+
+    @PostMapping("/login")
+    fun loginUsuario(@RequestBody login: Usuario): ResponseEntity<Usuario>{
+        return ResponseEntity.ok(login)
+    }
+
+    @PostMapping("/logout")
+    fun logoutUsuario(): ResponseEntity<String>{
+        return ResponseEntity.ok("Sesión cerrada")
+    }
+
+    @GetMapping("/me")
+    fun retriveUsuario(): ResponseEntity<Usuario>{
+        val meUsuario = Usuario(userName = "BTCloutt1", mail = "geovanimoxito@gmail.com", password = "geovanibbcito20")
+        return ResponseEntity.ok(meUsuario)
+    }
+
+}
