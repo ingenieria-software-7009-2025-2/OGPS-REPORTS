46af0a751cc3683e881788a1963267662ae0292a
GeovaniMtz <geovanimartinez14@ciencias.unam.mx>
2025-03-01 09:04:47 -0600

    Nuevo .sql con la bd sin poblar

---
+++ OGPS_REPORTS_db/PersonOGPS.sql
@@ -0,0 +1,55 @@
+CREATE TABLE person (
+    id_person SERIAL PRIMARY KEY,
+    token VARCHAR(255),
+    mail VARCHAR(255) NOT NULL UNIQUE CHECK (
+        mail ~* '^[a-zA-Z0-9._%+-]+@(gmail|outlook|hotmail)\.com$'
+    ),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL CHECK (
+        password ~ '^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$'
+    ),
+    role VARCHAR(50) NOT NULL CHECK (role IN ('Administrator', 'User'))
+);
+
+
+CREATE TABLE administrator (
+    id_admin SERIAL PRIMARY KEY,
+    id_person INT NOT NULL UNIQUE REFERENCES person(id_person) ON DELETE CASCADE
+);
+
+CREATE TABLE users (
+    id_user SERIAL PRIMARY KEY,
+    id_person INT NOT NULL UNIQUE REFERENCES person(id_person) ON DELETE CASCADE
+);
+
+CREATE TABLE incident (
+    id_incident SERIAL PRIMARY KEY,
+    id_user INT REFERENCES users(id_user) ON DELETE SET NULL,
+    id_admin INT REFERENCES administrator(id_admin) ON DELETE SET NULL,
+    latitude DECIMAL(9,6) NOT null,
+    longitude DECIMAL(9,6) NOT NULL,
+    category VARCHAR(50) NOT NULL CHECK (category IN ('Potholes and Defects', 'Street Lighting', 'Traffic Accidents', 'Obstacles', 'Other')),
+    description TEXT,
+    status VARCHAR(50) CHECK (status IN ('Reported', 'In Process', 'Resolved')) NOT NULL,
+    report_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE evidence (
+    id_evidence SERIAL PRIMARY KEY,
+    id_incident INT NOT NULL REFERENCES incident(id_incident) ON DELETE CASCADE,
+    url_photo TEXT NOT NULL
+);
+
+CREATE TABLE validation_request (
+    id_request SERIAL PRIMARY KEY,
+    id_user INT NOT NULL REFERENCES users(id_user) ON DELETE CASCADE,
+  	id_admin INT NOT NULL REFERENCES administrator(id_admin) ON DELETE cascade,
+  	id_evidence INT NOT NULL REFERENCES evidence(id_evidence) ON DELETE cascade,
+    quantity INT NOT NULL
+);
+
+
+
+
